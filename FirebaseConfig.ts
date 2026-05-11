2ccd7e582ba76ff9c582d016ff4eb3c01339adf0
Andrew <andrewtb02@gmail.com>
2026-05-11 19:58:38 +1000

    Revert "security: move Firebase keys to environment variables"
    This reverts commit 3b3a2590ae2d8a4ccc09829f3674ec2a92f37929.

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDk9xdNwBJw86bmGlLWxq5B1SJyKw9h54I",
+  authDomain: "cse3mad-assignmnet4.firebaseapp.com",
+  projectId: "cse3mad-assignmnet4",
+  storageBucket: "cse3mad-assignmnet4.firebasestorage.app",
+  messagingSenderId: "576432509599",
+  appId: "1:576432509599:web:a04de548109434796908aa",
+  measurementId: "G-WPM9JX5W7C"
 };
 
 // 1. Check if an app is already running before starting a new one
