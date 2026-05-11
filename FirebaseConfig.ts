80a39b7d89714a9943507f6215da14d118ea907b
Andrew <andrewtb02@gmail.com>
2026-05-11 19:59:10 +1000

    Revert "Firebase configuration and fix authentication crashes"
    This reverts commit 825ba016c37fe2c27c6074ae2b07298274f6bfe3.

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,28 +1,22 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { getApps, initializeApp } from "firebase/app";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { initializeApp } from "firebase/app";
 import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDk9xdNwBJw86bmGlLWxq5B1SJyKw9h54I",
-  authDomain: "cse3mad-assignmnet4.firebaseapp.com",
-  projectId: "cse3mad-assignmnet4",
-  storageBucket: "cse3mad-assignmnet4.firebasestorage.app",
-  messagingSenderId: "576432509599",
-  appId: "1:576432509599:web:a04de548109434796908aa",
-  measurementId: "G-WPM9JX5W7C"
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// 1. Check if an app is already running before starting a new one
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
-
-// 2. Export the correctly initialized services
+export const app = initializeApp(firebaseConfig);
 export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage)
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
-
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-export { app };
-
